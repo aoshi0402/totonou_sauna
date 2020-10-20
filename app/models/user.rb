0578814d38f26753842cedef1cb0e2afad723371
青島優 <aoshima.yu.0402@gmail.com>
2020-10-20 15:38:17 +0900

    フォロー、フォロワー機能実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,27 @@ class User < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
         
-        
-        
+  # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 自分がフォローする（与フォロー）側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+
+  # ユーザーをフォローする
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  # ユーザーをアンフォローする
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+    # フォローしているかを確認
+  def following?(user)
+    followings.include?(user)
+  end
 end
