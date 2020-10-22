52a92846424cac1b94c8899c67fd8cdf0b8759db
青島優 <aoshima.yu.0402@gmail.com>
2020-10-22 16:10:01 +0900

    いいね機能（非同期）実装

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,10 @@ class Review < ApplicationRecord
 
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+   # すでにいいねしているかを確認
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   attachment :image
   has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
 
   # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
