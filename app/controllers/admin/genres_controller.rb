9a7a75362bc06ed689416fc9bdf5c92155bc1423
青島優 <aoshima.yu.0402@gmail.com>
2020-11-05 17:28:55 +0900

    seedファイル作成、ジャンルデザイン実装

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -30,6 +30,6 @@ class Admin::GenresController < ApplicationController
   private
 
     def genre_params
-      params.require(:genre).permit(:name)
+      params.require(:genre).permit(:name, :image)
     end
 end
