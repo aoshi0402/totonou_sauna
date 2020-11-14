1c4e7205189c4ec38454919211120013a4eafec5
vagrant <vagrant@localhost.localdomain>
2020-11-14 07:52:53 +0000

    linked_dirs vendor

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,8 @@ set :application, "tototnou_sauna"
 set :repo_url, "git@github.com:aoshi0402/totonou_sauna.git"
 set :deploy_to, "/home/ec2-user/totonou_sauna"
 set :rbenv_ruby, '2.5.7'
-set :linked_files, %w{config/master.key .env vendor}
-append :linked_dirs, "log", "public", "tmp"
+set :linked_files, %w{config/master.key .env}
+append :linked_dirs, "log", "public", "tmp", "vendor"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
