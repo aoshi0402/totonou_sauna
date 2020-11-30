9d7cc477c366a85ba9f4f6aa350c15c5150da11b
青島優 <aoshima.yu.0402@gmail.com>
2020-11-30 13:49:02 +0900

    ページタイトル、絵文字実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
         if page_title.blank?
           base_title # トップページはタイトル
         else
-          "#{page_title} - #{base_title}" # トップ以外のページはタイトル「利用規約
+          "#{page_title} - #{base_title}" 
         end
     end
 
